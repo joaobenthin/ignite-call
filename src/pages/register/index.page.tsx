96e505bfaef6d67e0a3f500bd65cf356e9c0af33
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2024-02-16 12:36:33 -0300

    Persistindo usuário no Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,11 +1,12 @@
+import { useEffect } from 'react'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { Container, Form, FormError, Header } from './styles'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { api } from '@/lib/axios'
+import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,7 +42,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
