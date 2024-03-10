f30a0ee9ae1f4c5837e2b51d5c63a82acf5fa281
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2024-03-10 17:20:47 -0300

    Formulário de disponibilidade

---
+++ src/utils/get-week-days.ts
@@ -0,0 +1,16 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', {
+    weekday: 'long',
+  })
+
+  return Array.from(Array(7).keys()).map((day) =>
+    formatter
+      .format(new Date(Date.UTC(2024, 3, day)))
+      .localeCompare((weekDay) => {
+        return weekDay
+          .substring(0, 1)
+          .toUpperCase()
+          .concat(weekDay.substring(1))
+      }),
+  )
+}
